59db969d8d9d58b55e25d5bf0cffa90ff49e3bab
darkj0k <rybushkin09@bk.ru>
2024-03-19 20:40:27 +0400

    Initial commit

---
+++ Assets/Scripts/MovementCharacter.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    private void Move()
+    {
+        while (true)
+        {
+            UnityEngine.Input.
+        }
+    }
+    // Update is called once per frame
+    void Start()
+    {
+        Move();
+    }
+}
